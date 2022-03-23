d809f377a7a0b1d2948cbe0c25ce6ff5da737016
Aaron Tran <altran32@gmail.com>
2022-03-22 23:05:28 -0700

    modify for prod

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 /* eslint-disable no-console */
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
 
 const port = 3004;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.static(`${__dirname}/../client/dist`));
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World');
+})
 
 app.listen(port, () => { console.log(`Successfully running on port ${port}`); });
