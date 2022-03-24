839ad9e082649037c376588b652c29df87240d5a
Aaron Tran <altran32@gmail.com>
2022-03-23 17:42:35 -0700

    rm lint install proptypes. install proptypes. destructure props in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,14 +4,12 @@ import ImageListItem from '@mui/material/ImageListItem';
 import ImageList from '@mui/material/ImageList';
 import AboutPic from '../images/AboutUs.png';
 
-function About(props) {
-  const { title, description } = props.data;
-
+function About({ data: { title, description } }) {
   return (
     <Container>
       <ImageList cols={1}>
         <ImageListItem>
-          <img src={AboutPic} alt="AboutUs" loading="lazy" style={{ paddingTop: 10}} />
+          <img src={AboutPic} alt="AboutUs" loading="lazy" style={{ paddingTop: 10 }} />
         </ImageListItem>
       </ImageList>
       <h3>{title}</h3>
@@ -22,3 +20,4 @@ function About(props) {
 }
 
 export default About;
+
