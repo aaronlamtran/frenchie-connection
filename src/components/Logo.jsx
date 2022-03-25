fcc9173c153fd6c7460820f0641b1fd16eabdf12
Aaron Tran <altran32@gmail.com>
2022-03-25 00:24:54 -0700

    on logo click takes user to root dir

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from '../images/logo.png';
+import React from "react";
+import logo from "../images/logo.png";
 
 function Logo() {
-  return <img src={logo} alt="Logo" />;
+  return (
+    <img src={logo} alt="Logo" onClick={() => (window.location.href = "/")} />
+  );
 }
 
 export default Logo;
--- src/components/LogoText.jsx
+++ src/components/LogoText.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import logoText from '../images/logoText1.png';
+import React from "react";
+import logoText from "../images/logoText1.png";
 
 function LogoText() {
-  return <img src={logoText} alt="Logo" />;
+  return (
+    <img
+      src={logoText}
+      alt="Logo"
+      onClick={() => (window.location.href = "/")}
+    />
+  );
 }
 
 export default LogoText;
