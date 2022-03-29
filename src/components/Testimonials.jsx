28347c87c57beed6906ec4b44876304a7c92f895
Aaron Tran <altran32@gmail.com>
2022-03-28 20:15:12 -0700

    add padding to testimonial containers

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -7,12 +7,12 @@ import Paper from "@mui/material/Paper";
 
 function Testimonials({ data: { title, entries } }) {
   return (
-    <Container id="Testimonials" >
+  <Container id="Testimonials">
       <h3>{title}</h3>
       <Grid container sx={{ display: "grid", gap: 1.5, alignItems: "center"}}>
         {entries.map((person, idx) => (
-          <Grid item xs={12} sm={10} md={8} key={person.name}>
-            <Paper>
+          <Grid item xs={12} sm={9} md={9} key={person.name}>
+            <Paper  sx={{padding: 1.5}}>
               <Stack direction="column" spacing={0.5}>
                 <Stack direction="row" spacing={0.5}>
                   <AvatarReview data={{ ...person, idx }} />
