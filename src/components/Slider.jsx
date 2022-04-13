24425f37d51e545bb0cabd17d8c2e68092e92b51
Aaron Tran <altran32@gmail.com>
2022-04-12 22:00:38 -0700

    add sample images. add image-modal

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Slider.css";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import Container from "@mui/material/Container";
-// import Typography from "@mui/material/Typography";
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 
 function CardSlider(props) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [images, setImages] = useState([
+    "//placekitten.com/1500/500",
+    "//placekitten.com/4000/3000",
+    "//placekitten.com/800/1200",
+    "//placekitten.com/1500/1500"
+  ]);
+  const [photoIndex, setPhotoIndex] = useState(0);
   const slideLeft = () => {
     var slider = document.getElementById("slider");
     slider.scrollLeft = slider.scrollLeft + 320;
   };
-
   const slideRight = () => {
     var slider = document.getElementById("slider");
     slider.scrollLeft = slider.scrollLeft - 320;
   };
 
-  const handleSliderClick = (slider) => {
-    console.log("this is going to open pup details");
+  const handleSliderClick = (slide) => {
+    // e.preventDefault();
+    console.log({slide})
+    setImages(slide.largeImages)
+    setIsOpen(!isOpen);
   };
 
   return (
     <Container sx={{ pb: 2, padding: 0.25}} id="Pups">
       {/* <Typography variant="h5">Pups</Typography> */}
+      {isOpen && (
+          <Lightbox
+            mainSrc={images[photoIndex]}
+            nextSrc={images[(photoIndex + 1) % images.length]}
+            prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+            onCloseRequest={()=> setIsOpen(false)}
+            onMovePrevRequest={() =>
+                setPhotoIndex((photoIndex + images.length - 1) % images.length)
+            }
+            onMoveNextRequest={() =>
+              setPhotoIndex((photoIndex + 1) % images.length)
+            }
+          />
+        )}
       <div id="main-slider-container">
         <ArrowBackIosNewIcon
           className="slider-icon left"
@@ -34,7 +59,7 @@ function CardSlider(props) {
               <div
                 className="slider-card"
                 key={index + 1}
-                onClick={() => handleSliderClick()}
+                onClick={()=> handleSliderClick(slide)}
               >
                 <div
                   className="slider-card-image"
