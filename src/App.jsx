92f9160555df903356ac3629bab6d1440865728e
Aaron Tran <altran32@gmail.com>
2022-04-18 15:04:57 -0700

    add alert view

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import "./App.css";
 import SmoothScroll from "smooth-scroll";
 import CardSlider from "./components/Slider";
 import Router from "./Router";
+import AlertsView from './utils/AlertsView';
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
@@ -39,36 +40,66 @@ const theme = createTheme({
 });
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       successMessages: [],
-      errorMessages: []
-    }
+      errorMessages: [],
+    };
   }
-render(){
 
-  return (
-    <ThemeProvider theme={theme}>
-      {/* <Container sx={{ padding: 0.5 }}> */}
-      <Container sx={{ padding: 0.25}} >
-        <Nav />
-        <Router/>
-        <About data={aboutData} />
-        <CardSlider slides={galleryData} />
-        <Box
-          sx={{ display: "flex", flexDirection: { xs: "column", md: "row" } }}
+  handleAddErrorMessages = (errors) => {
+    this.setState({ errorMessages: [...this.state.errorMessages, ...errors] });
+  };
+
+  handleAddSuccessMessage = (successMessage) => {
+    this.setState({
+      successMessages: [...this.state.successMessages, { msg: successMessage }],
+    });
+  };
+
+  handleDismissErrorMessage = (index) => {
+    const errorMessages = [...this.state.errorMessages];
+    errorMessages.splice(index, 1);
+    this.setState({ errorMessages: [...errorMessages] });
+  };
+
+  handleDismissSuccessMessage = (index) => {
+    const successMessages = [...this.state.successMessages];
+    successMessages.splice(index, 1);
+    this.setState({ successMessages: [...successMessages] });
+  };
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        {/* <Container sx={{ padding: 0.5 }}> */}
+        <Container sx={{ padding: 0.25 }}>
+          <Nav />
+          <AlertsView
+            successMessages={this.state.successMessages}
+            errorMessages={this.state.errorMessages}
+            handleDismissSuccessMessage={this.handleDismissSuccessMessage}
+            handleDismissErrorMessage={this.handleDismissErrorMessage}
+          />
+          <Router
+            handleAddErrorMessages={this.handleAddErrorMessages}
+            handleAddSuccessMessage={this.handleAddSuccessMessage}
+          />
+          <About data={aboutData} />
+          <CardSlider slides={galleryData} />
+          <Box
+            sx={{ display: "flex", flexDirection: { xs: "column", md: "row" } }}
           >
-          <Testimonials data={testimonialData} />
-          <Container sx={{ padding: 0.25}}>
-            <FAQ data={FAQ_data} />
-            <Contact data={contactData} />
-          </Container>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  );
-}
+            <Testimonials data={testimonialData} />
+            <Container sx={{ padding: 0.25 }}>
+              <FAQ data={FAQ_data} />
+              <Contact data={contactData} />
+            </Container>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
--- src/components/dogs/Dogs.jsx
+++ src/components/dogs/Dogs.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Container } from "reactstrap";
 import { withRouter } from "react-router-dom";
 import axios from "axios";
+import dayjs from "dayjs";
 
 class Dogs extends Component {
   state = {
