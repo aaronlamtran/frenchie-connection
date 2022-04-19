26856efcbbf819a25e2759cedd1e33f0f0db8f30
Aaron Tran <altran32@gmail.com>
2022-04-19 14:22:25 -0700

    format table

--- src/components/dogs/WaitlistTable.jsx
+++ src/components/dogs/WaitlistTable.jsx
@@ -1,41 +1,48 @@
 import React from "react";
-import { Table } from "reactstrap";
 import dayjs from "dayjs";
-import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 
 const WaitlistTableView = ({ waitlists, dogInfo }) => (
-  <Box>
-
-    <div>Name: {dogInfo.dogName}</div>
-    <div>Description: {dogInfo.dogDescription}</div>
-    <div>
+  <TableContainer componet={Paper}>
+    <Typography>Name: {dogInfo.dogName}</Typography>
+    <Typography>Description: {dogInfo.dogDescription}</Typography>
+    <Typography>
       updated at:{dayjs(dogInfo.updatedAt).format("DD-MM-YYYY hh:mm A")}
-    </div>
-    <Table striped>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          {/* <th>Waitlist</th> */}
-          <th>Phone</th>
-          <th>Email</th>
-        </tr>
-      </thead>
-      <tbody>
+    </Typography>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>#</TableCell>
+          <TableCell>Name</TableCell>
+          {/* <TableCell>Waitlist</TableCell> */}
+          <TableCell>Phone</TableCell>
+          <TableCell>Email</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
         {waitlists.map((waitlist, index) => (
           // <tr key={waitlist._id}>
-          <tr key={index}>
-            <th scope="row">{index + 1}</th>
+          <TableRow key={index}>
+            <TableCell scope="row" component="th">
+              {index + 1}
+            </TableCell>
             {console.log(waitlist)}
-            <td>{waitlist.name}</td>
-            {/* <td>{waitlist.waitlistPosition}</td> */}
-            <td>{waitlist.phone ? waitlist.phone : "N/A" }</td>
-            <td>{waitlist.email}</td>
-          </tr>
+            <TableCell>{waitlist.name}</TableCell>
+            {/* <TableCell>{waitlist.waitlistPosition}</TableCell> */}
+            <TableCell>{waitlist.phone ? waitlist.phone : "N/A"}</TableCell>
+            <TableCell>{waitlist.email}</TableCell>
+          </TableRow>
         ))}
-      </tbody>
+      </TableBody>
     </Table>
-  </Box>
+  </TableContainer>
 );
 
 export default WaitlistTableView;
