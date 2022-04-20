a1332c848a07ffb81915ea98c82d4f9c7243d3ed
Aaron Tran <altran32@gmail.com>
2022-04-19 19:55:28 -0700

    margin

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageList from "@mui/material/ImageList";
 import AboutPic from "../images/AboutUs.png";
@@ -8,20 +9,32 @@ import Typography from "@mui/material/Typography";
 
 function About({ data: { title, description } }) {
   return (
-    <Paper sx={{ padding: 0.25, marginBottom: 1, paddingBottom: 5, marginTop:1 }}>
-      <Container id="About">
-        <ImageList cols={1}>
-          <ImageListItem>
-            <img src={AboutPic} alt="AboutUs" loading="lazy" />
-          </ImageListItem>
-        </ImageList>
-        <Typography variant="h5">{title}</Typography>
-        <br />
-        <Typography >{description}</Typography>
+    <Box sx={{marginTop: 1,}}>
+      <Paper
+        sx={{ padding: 0.25, marginBottom: 1, paddingBottom: 5, marginTop: 1,maxWidth: { md: 800 }, margin: 'auto'}}
+      >
+        <Container>
+          <ImageList cols={1}>
+            <ImageListItem>
+              <img src={AboutPic} alt="AboutUs" loading="lazy" />
+            </ImageListItem>
+          </ImageList>
+          <br />
+          <Box
+            sx={{
+              maxWidth: { md: 800 },
+              margin: "auto",
+              marginTop: 1,
+            }}
+          >
+            <Typography variant="h5">{title}</Typography>
+            <Typography>{description}</Typography>
+          </Box>
 
-        <Container />
-      </Container>
-    </Paper>
+          <Container />
+        </Container>
+      </Paper>
+    </Box>
   );
 }
 
