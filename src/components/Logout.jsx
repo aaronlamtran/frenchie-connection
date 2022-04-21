0de022b9de844f752e8444ea205a296190186935
Aaron Tran <altran32@gmail.com>
2022-04-20 21:44:07 -0700

    fix navigation on logout

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { signOut, getAuth } from "firebase/auth";
 import frenchie from "../config/firebase-config";
 import Button from "@mui/material/Button";
+import {useNavigate} from 'react-router-dom';
 
 
 export default function Logout() {
+  const navigate = useNavigate();
 
   const authentication = getAuth(frenchie);
   const logoutOfGoogle = () => {
     signOut(authentication)
       .then(() => {
         console.log("logged out successfully");
-
+        navigate('/')
       })
       .catch((error) => {
         console.log({ error });
